b13e3a1673aba95daa786e85d3d28504bbb0f1d8
byeongcheoljo <max91128@naver.com>
2020-07-29 11:37:25 +0900

    Update BertEmbedding.py

--- BertEmbedding.py
+++ BertEmbedding.py
@@ -4,7 +4,7 @@ BERT input :
 - [CLS], [SEP]
 - BERT vocab 안에 있는 token
 - BERT tokenizer의 token ID
-- mask ID  (뭐가 봐도 되는 토큰이고 아닌지) 1 or 0 : padding 0 , not padding 1
+- mask ID  1 or 0 : padding 0 , not padding 1
 - segment ID (문장 구분) : input sentence가 한 문장이면 0, 두 문장일 때 : 첫문장은 0, 다음문장은 1
 - positional embeddings (한 문장 내 특정 토큰의 순서)
 
